591b2799b5ffbbcbad397c316b30c8a7aa9e7b72
Cecilia <cecilia.svensson.is@gmail.com>
2020-04-27 11:54:20 +0200

    added failing code but need helt

--- chat/src/components/Room/NewRoomModal.js
+++ chat/src/components/Room/NewRoomModal.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import ReactDOM form 'react-dom';
+import ReactDOM from 'react-dom';
 
 export default function NewRoomModal (){
-    return ReactDOM.createPortal(())
+    return ReactDOM.createPortal((
+        <div className='createNewRoomModal'>
+            <h3>Create a new room</h3>
+            <label htmlFor='createNewRoomInput'> Roomname:
+                <input type='text' id='createNewRoomInput' placeholder='Roomname' />
+            </label>
+        </div>
+    ), document.body);
 }
--- chat/src/components/Room/Room.js
+++ chat/src/components/Room/Room.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Helmet} from 'react-helmet-async';
 import {Router, Link} from 'react-router-dom';
-import './Room.css'
+import './Room.css';
+import NewRoomModal from './NewRoomModal';
 
 export default function Room (){
+    const [createRoomModal, setCreateRoomModal] = useState(false);
+    const [roomname, setRoomname] = useState('');
+
     function createNewRoom(){
-        
+        setCreateRoomModal(true)
     }
+    
+    function onSubmit(e){
+        e.preventDefault()
+
+    }
+
     return(
         <div className='roomContainer' >
             <Helmet>
                 <title>Room</title>
             </Helmet>
             <div>
-                <form onSubmit='onSubmit'>
+                <form onSubmit={onSubmit}>
                     <label htmlFor='username' className='inputfieldlabel'> Username: 
                         <input type='text' id='username' placeholder='Enter username'></input>
                     </label>
@@ -24,8 +34,9 @@ export default function Room (){
                     <br/>
                     <button type='submit'>Enter room</button>
                 </form>
-                <button className='createNewRoom' onClick='createNewRoom'>Create new room</button>
+                <button className='createNewRoom' onClick={createNewRoom(createRoomModal, setCreateRoomModal, )}>Create new room</button>
             </div>
+            {createRoomModal && <NewRoomModal room={roomname} setCreateRoomModal={setCreateRoomModal} /> }
         </div>
     )
 }
